5b27bea2c790dca55be9cd1328d1bd8506e70003
Aniket Prakash <b218004@iiit-bh.ac.in>
2021-08-18 21:20:45 +0530

    feat: add separate validation for inputs

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const cashPaid = document.querySelector("#cash-paid");
 const checkButton = document.querySelector("#btn-check");
 const notesNumber = document.querySelectorAll(".notes-num");
 const message = document.querySelector("#err-msg");
+const btnProceed = document.querySelector("#btn-proceed");
+const changeTable = document.querySelector("#change-table");
+const cashDiv = document.querySelector("#cash")
+
 const availableNotes = [2000,500,100,20,10,5,1];
+
+btnProceed.addEventListener("click",()=>{
+    var billAmountValue = Number(billAmount.value);
+    hideMsg();
+    if(billAmountValue > 0){
+        btnProceed.style.display = "none";
+        cashDiv.style.display = "block";
+    }else{
+        showMsg("Enter a valid bill amount");
+    }
+})
+
 
 checkButton.addEventListener("click",()=>{
     var billAmountValue = Number(billAmount.value);
@@ -12,17 +28,21 @@ checkButton.addEventListener("click",()=>{
     if (billAmountValue > 0){
         if(billAmountValue < cashPaidValue){
             var diffAmount = cashPaidValue - billAmountValue;
+            changeTable.style.display = "block";
             calculateChange(diffAmount);
         }
         else if(cashPaidValue == billAmountValue){
+            changeTable.style.display = "none";
             showMsg("No change required");
         }
         else{
+            changeTable.style.display = "none";
             showMsg("Cash paid should be atleast equal to the bill amount");
         }
     }
     else{
-        showMsg("Enter a valid amount");
+        changeTable.style.display = "none";
+        showMsg("Enter a valid bill amount");
     }
 })
 
